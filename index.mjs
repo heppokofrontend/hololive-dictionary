9555a3d50ef7eeb984d1d09c35e958a6fb85f587
Ecma Kawarabe <heppokofrontend@gmail.com>
2021-06-13 10:51:24 +0900

    :zap: データの持ち方を修正

--- index.mjs
+++ index.mjs
@@ -1,18 +1,29 @@
 import fs from 'fs';
 import dict from './src/dict.mjs';
 
-const forMac = dict.map(item => item.join(',')).join('\n');
-const forMSIME = dict.map(item => item.join('\t')).join('\n');
-const forGoogleIME = (() => {
+/** [string, string, string][] を作る */
+const parsed = (() => {
+  return dict.map(({name}) => [
+    [
+      name[0],
+      name[1],
+      '人名',
+    ],
+  ]);
+})().flat();
+const CSV = parsed.map(item => item.join(',')).join('\n');
+const TSV = parsed.map(item => item.join('\t')).join('\n');
+const googleIME = (() => {
   const callback = c => String.fromCharCode(c.charCodeAt(0) + 0x60);
 
-  return dict.map(item => {
+  // 読み仮名をカタカナに変換する
+  return parsed.map(item => {
     item[0] = item[0].replace(/[ぁ-ん]/g, callback);
 
     return item.join('\t');
   }).join('\n');
 })();
 
-fs.writeFileSync('./dist/mac/mac-ime-dict.txt', forMac);
-fs.writeFileSync('./dist/win/ms-ime-dict.txt', forMSIME);
-fs.writeFileSync('./dist/win/google-ime-dict.txt', forGoogleIME);
+fs.writeFileSync('./dist/mac/mac-ime-dict.txt', CSV);
+fs.writeFileSync('./dist/win/ms-ime-dict.txt', TSV);
+fs.writeFileSync('./dist/win/google-ime-dict.txt', googleIME);
