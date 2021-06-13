6cf19acf194dde85a1b90789672eccebd731f406
Ecma Kawarabe <heppokofrontend@gmail.com>
2021-06-13 21:57:34 +0900

    :zap: 書き出し順を変更

--- index.ts
+++ index.ts
@@ -3,27 +3,26 @@ import path from 'path';
 import {dictionary} from './src/dict';
 
 /**
- * ? 品詞について
+ * 品詞について
+ *
  * WinIME - 「名詞」「人名」「地名」「短縮よみ」「顔文字」「その他」
  * Mac - 「普通名詞」「サ変名詞」「人名」「地名」「形容詞」「副詞」「接尾語」「動詞」その他すべての品詞
  */
 type PartsOfSpeech = '普通名詞' | 'サ変名詞' | '人名' | '地名' | '形容詞' | '副詞' | '接尾語' | '動詞';
+/** 辞書データ１行分。[よびがな、語句、品詞] */
+type WordSet = [string, string, PartsOfSpeech];
 
-
-const parsed: [string, string, PartsOfSpeech][] = (() => {
-  const single = (data: [string, string], type: PartsOfSpeech): [string, string, PartsOfSpeech] => {
-    return [
-      data[0],
-      data[1],
-      type,
-    ];
-  };
+/**
+ * dict.tsに書かれた内容を整形する
+ */
+const dataSet: WordSet[] = (() => {
+  /** 1つの読みに対して語句が複数ある */
   // const multi = (name, marks = [], type) => {
-  //   const result = [];
+  //   const result: WordSet[] = [];
 
   //   for (const mark of marks) {
   //     result.push([
-  //       `＠${name[0]}`,
+  //       `＠${name}`,
   //       mark,
   //       type,
   //     ]);
@@ -33,26 +32,28 @@ const parsed: [string, string, PartsOfSpeech][] = (() => {
   // };
 
   return dictionary.map(({name, marks}) => {
-    const data: [string, string, PartsOfSpeech][] = [];
-    const nameYomi = name[0]?.split(/\s/);
-    const nameKaki = name[1]?.split(/\s/);
+    const data: WordSet[] = [];
+    const nameYomi = [name[0].replace(/\s/g, ''), ...name[0]?.split(/\s/)];
+    const nameKaki = [name[1].replace(/\s/g, ''), ...name[1]?.split(/\s/)];
 
     // 名前
-    data.push(single([nameYomi[0], nameKaki[0]], '人名'));
-    data.push(single([nameYomi[1], nameKaki[1]], '人名'));
-    data.push(single([nameYomi.join(''), nameKaki.join('')], '人名'));
-    // data.push(...multi(name, marks, '人名'));
+    data.push([nameYomi[0], nameKaki[0], '人名']);
+    data.push([nameYomi[1], nameKaki[1], '人名']);
+    data.push([nameYomi[2], nameKaki[2], '人名']);
+
+    // その他の情報
+    // data.push(...multi(nameYomi[0], marks, '人名'));
 
     return data;
   });
 })().flat().filter((item: any) => !!item[0]);
-const CSV = parsed.map(item => item.join(',')).join('\n');
-const TSV = parsed.map(item => item.join('\t')).join('\n');
+const CSV = dataSet.map(item => item.join(',')).join('\n');
+const TSV = dataSet.map(item => item.join('\t')).join('\n');
 const googleIME = (() => {
   const callback = (c: string) => String.fromCharCode(c.charCodeAt(0) + 0x60);
 
   // 読み仮名をカタカナに変換する
-  return parsed.map(item => {
+  return dataSet.map(item => {
     item[0] = item[0].replace(/[ぁ-ん]/g, callback);
 
     return item.join('\t');
