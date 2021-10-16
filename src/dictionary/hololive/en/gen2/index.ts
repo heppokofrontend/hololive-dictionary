60e59895db2e94cc2bb35f34c57ee5a519b1da6a
heppokofrontend <heppokofrontend@gmail.com>
2021-10-17 03:07:59 +0900

    Add EN Gen 2

---
+++ src/dictionary/hololive/en/gen2/index.ts
@@ -0,0 +1,13 @@
+import { ceresFauna } from "./ceres-fauna";
+import { tsukumoSana } from "./tsukumo-sana";
+import { hakosBaelz } from "./hakos-baelz";
+import { nanashiMumei } from "./nanashi-mumei";
+import { ouroKronii } from "./ouro-kronii";
+
+export const hololiveEnGen2 = [
+  ceresFauna,
+  tsukumoSana,
+  hakosBaelz,
+  nanashiMumei,
+  ouroKronii,
+];
--- src/dictionary/hololive/en/index.ts
+++ src/dictionary/hololive/en/index.ts
@@ -1,7 +1,9 @@
 import { hololiveEnGen1 } from "./gen1";
 import { hololiveEnGen1_5 } from "./gen1.5";
+import { hololiveEnGen2 } from "./gen2";
 
 export const hololiveEn: LiverData[] = [
   ...hololiveEnGen1,
   ...hololiveEnGen1_5,
+  ...hololiveEnGen2,
 ];
