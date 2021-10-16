77423dfd6d7115f6a5b8051015fedfbee1f39ede
heppokofrontend <heppokofrontend@gmail.com>
2021-10-17 02:41:43 +0900

    Add Ceres fauna

---
+++ src/dictionary/hololive/en/gen2/ceres-fauna.ts
@@ -0,0 +1,16 @@
+export const ceresFauna: LiverData = {
+  name: ['せれす ふぁうな', 'セレス ファウナ'],
+  alias: [
+    ['せれす', 'Ceres'],
+    ['ファウナ', 'Fauna'],
+    ['せれすふぁうな', 'Ceres Fauna'],
+  ],
+  marks: ['🌿'],
+  tags: [
+    '#faunline',
+    '#FineFaunart',
+    '#Saplings',
+  ],
+  fans: ['Saplings'],
+  twitter: ['@ceresfauna'],
+};
