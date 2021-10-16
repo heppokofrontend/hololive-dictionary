4f322a6c95198ae834af27d6dbe1d07f2d5e2232
heppokofrontend <heppokofrontend@gmail.com>
2021-10-17 03:05:04 +0900

    Add Ouro Kronii

---
+++ src/dictionary/hololive/en/gen2/ouro-kronii.ts
@@ -0,0 +1,19 @@
+export const ouroKronii: LiverData = {
+  name: ['おーろ くろにー', 'オーロ クロニー'],
+  alias: [
+    ['おーろ', 'Ouro'],
+    ['くろにー', 'Kronii'],
+    ['おーろくろにー', 'Ouro Kronii'],
+  ],
+  marks: ['⏳'],
+  tags: [
+    '#kronillust',
+    '#krotime',
+    '#クロニーラ',
+    '#kroniijokes',
+  ],
+  fans: ['Kronies'],
+  twitter: ['@ourokronii'],
+  others: [
+  ],
+};
