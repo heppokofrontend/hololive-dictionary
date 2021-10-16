16b28a24205c335ccf799382c9fb108fed8937fe
heppokofrontend <heppokofrontend@gmail.com>
2021-10-17 02:56:22 +0900

    Add Hakos Baelz

---
+++ src/dictionary/hololive/en/gen2/hakos-baelz.ts
@@ -0,0 +1,21 @@
+export const hakosBaelz: LiverData = {
+  name: ['はこす べーるず', 'Hakos Baelz'],
+  alias: [
+    ['はこす', 'Hakos'],
+    ['べーるず', 'Baelz'],
+    ['はこすべーるず', 'Hakos Baelz'],
+  ],
+  marks: ['🎲'],
+  tags: [
+    '#enterbaelz',
+    '#ベール登場',
+    '#illustrayBAE',
+    '#ベーアート',
+    '#MADDOOFS',
+  ],
+  fans: ['BAERATS'],
+  twitter: ['@hakosbaelz'],
+  others: [
+    ['でっどびーつ', 'Dead Beats'],
+  ],
+};
