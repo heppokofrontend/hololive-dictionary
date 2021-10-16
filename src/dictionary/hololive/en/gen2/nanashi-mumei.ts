8db648cc8f4b2dc0b55f38bd0725f96b723b7677
heppokofrontend <heppokofrontend@gmail.com>
2021-10-17 02:59:55 +0900

    Add Nanashi Mumei

---
+++ src/dictionary/hololive/en/gen2/nanashi-mumei.ts
@@ -0,0 +1,18 @@
+export const nanashiMumei: LiverData = {
+  name: ['ななし むめい', '七詩 ムメイ'],
+  alias: [
+    ['ななし', 'Nanashi'],
+    ['むめい', 'Mumei'],
+    ['ななしむめい', 'Nanashi Mumei'],
+  ],
+  marks: ['🪶'],
+  tags: [
+    '#watchMEI',
+    '#drawMEI',
+    '#memeMEI',
+  ],
+  fans: ['Hoomans'],
+  twitter: ['@nanashimumei_en'],
+  others: [
+  ],
+};
