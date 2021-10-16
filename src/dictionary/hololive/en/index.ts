de3f89518c73df7da2f6b0fc6065ef2d892b0f34
heppokofrontend <heppokofrontend@gmail.com>
2021-10-17 03:32:39 +0900

    Add Omegaα

--- src/dictionary/hololive/en/index.ts
+++ src/dictionary/hololive/en/index.ts
@@ -3,6 +3,19 @@ import { hololiveEnGen1_5 } from "./gen1.5";
 import { hololiveEnGen2 } from "./gen2";
 
 export const hololiveEn: LiverData[] = [
+  {
+    name: ['おめが あるふぁ', 'オメガα'],
+    alias: [
+      ['おめがあるふぁ', 'Omegaα'],
+    ],
+    marks: [],
+    tags: [
+      '#omegallery',
+      '#omegarchive',
+    ],
+    fans: [],
+    twitter: ['@omegaalpha_en'],
+  },
   ...hololiveEnGen1,
   ...hololiveEnGen1_5,
   ...hololiveEnGen2,
