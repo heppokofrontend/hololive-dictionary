ea098618b970c551b3cc264273d7cde6db549404
heppokofrontend <heppokofrontend@gmail.com>
2021-10-17 03:07:23 +0900

    Add Tsukumo Sana

---
+++ src/dictionary/hololive/en/gen2/tsukumo-sana.ts
@@ -0,0 +1,17 @@
+export const tsukumoSana: LiverData = {
+  name: ['つくも さな', '九十九 佐命'],
+  alias: [
+    ['つくも', 'Tsukumo'],
+    ['さな', 'Sana'],
+    ['つくもさな', 'Tsukumo Sana'],
+  ],
+  marks: ['🪐'],
+  tags: [
+    '#galaxillust',
+    '#SanaLanding',
+  ],
+  fans: ['Sanallite'],
+  twitter: ['@tsukumosana'],
+  others: [
+  ],
+};
