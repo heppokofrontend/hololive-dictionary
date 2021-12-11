c4c1ffd89b0aad97f9bb95624416c71cb7b06e71
heppokofrontend <heppokofrontend@gmail.com>
2021-12-11 21:15:32 +0900

    Add Sakamata Chloe

---
+++ src/dictionary/hololive/jp/gen6/sakamata-chloe.ts
@@ -0,0 +1,21 @@
+export const sakamataChloe: LiverData = {
+  name: ['さかまた くろえ', '沙花叉 クロヱ'],
+  alias: [
+    ['くろたん', 'クロたん'],
+    ['しんじん', 'しんじん'],
+    ['しんじん', '新人'],
+    ['かんぶ', '幹部'],
+  ],
+  marks: ['🎣'],
+  tags: [
+    '#またまたさかまた', // 配信タグ
+    '#さかまた飼育日記', // ファンアート
+  ],
+  fans: ['飼育員'],
+  twitter: ['@sakamatachloe'],
+  others: [
+  ],
+  flags: {
+    activity: '活動中',
+  },
+};
