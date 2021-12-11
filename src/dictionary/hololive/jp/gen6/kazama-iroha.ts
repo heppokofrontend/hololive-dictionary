196ac7907e5b582598380f055e373e6a24b37793
heppokofrontend <heppokofrontend@gmail.com>
2021-12-11 20:58:33 +0900

    Add Kazama Iroha

---
+++ src/dictionary/hololive/jp/gen6/kazama-iroha.ts
@@ -0,0 +1,20 @@
+export const kazamaIroha: LiverData = {
+  name: ['かざま いろは', '風真 いろは'],
+  alias: [
+    ['さむらい'],
+  ],
+  marks: ['🍃'],
+  tags: [
+    '#かざま修行中', // 生放送
+    '#いろはにも絵を', // ファンアート
+    '#かざまが斬る', // 切り抜き
+  ],
+  fans: ['かざま隊'],
+  twitter: ['@kazamairohach'],
+  others: [
+    ['いろはにもえを', 'いろはにも絵を'],
+  ],
+  flags: {
+    activity: '活動中',
+  },
+};
