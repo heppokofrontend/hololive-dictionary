90a983f8f41d22680aaccc7ab714989bfcdc2847
heppokofrontend <heppokofrontend@gmail.com>
2023-06-26 03:49:58 +0900

    Update Holostars JP to latest informations

--- src/dictionary/holostars/jp/gen1/yakushiji-suzaku.ts
+++ src/dictionary/holostars/jp/gen1/yakushiji-suzaku.ts
@@ -1,11 +1,11 @@
 export const yakushijiSuzaku: LiverData = {
   name: ['やくしじ すざく', '薬師寺 朱雀'],
-  alias: [['', '']],
+  alias: [],
   marks: ['💊'],
   tags: [
-    '#薬師寺朱雀', // 全般
-    '#朱雀絵', // ファンアート
-    '#朱雀動画', // 切り抜き
+    ['#薬師寺朱雀', '全般'],
+    ['#朱雀絵', 'ファンアート'],
+    ['#朱雀動画', '切り抜き'],
   ],
   fans: ['ヤクナー'],
   twitter: ['@YakushijiSuzaku'],
--- src/dictionary/holostars/jp/index.ts
+++ src/dictionary/holostars/jp/index.ts
@@ -3,6 +3,20 @@ import { holostarsJpGen2 } from './gen2';
 import { holostarsJpGen3 } from './gen3';
 
 export const holostarsJp: LiverData[] = [
+  {
+    name: ['だいどう しのゔ', '大道 シノヴ'],
+    alias: [],
+    marks: [],
+    tags: [
+      ['#大道シノヴ', '全般'],
+      ['#押忍絵', 'ファンアート'],
+    ],
+    fans: ['ホロスタ民'],
+    twitter: ['@daidoushinove'],
+    flags: {
+      isActive: false,
+    },
+  },
   ...holostarsJpGen1,
   ...holostarsJpGen2,
   ...holostarsJpGen3,
