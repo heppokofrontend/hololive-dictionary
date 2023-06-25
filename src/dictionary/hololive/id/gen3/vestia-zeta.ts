ee7c9f2a4ef53302870ec9676d6c517d990cba53
heppokofrontend <heppokofrontend@gmail.com>
2023-06-26 01:51:49 +0900

    Add hololive ID gen3

---
+++ src/dictionary/hololive/id/gen3/vestia-zeta.ts
@@ -0,0 +1,15 @@
+export const vestiaZeta: LiverData = {
+  name: ['ヴぇすてぃあ ぜーた', 'Vestia Zeta'],
+  alias: [['', '']],
+  marks: ['📜'],
+  tags: [
+    '#VestiaZeta', // 一般
+    '#Zecretary', // ファンネーム
+    '#ZecretLive', // 配信
+    '#Zetacrylic', // ファンアート
+    '#Zetassets', // ファンアート(素材提供)
+    '#DEEZeta', // ミーム
+  ],
+  fans: ['Zecretary', 'Secret Unit'],
+  twitter: ['@vestiazeta'],
+};
