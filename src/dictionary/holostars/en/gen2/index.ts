29f28b3603d0460611017a5d157f048d8bcf11a8
heppokofrontend <heppokofrontend@gmail.com>
2023-06-26 04:37:26 +0900

    Add Holostar EN gen2

--- src/dictionary/holostars/en/gen2/index.ts
+++ src/dictionary/holostars/en/gen2/index.ts
@@ -1,1 +1,11 @@
-export const holostarsEnGen2: LiverData[] = [];
+import { banzoinHakka } from './banzoin-hakka';
+import { gavisBettel } from './gavis-bettel';
+import { josuijiShinri } from './josuiji-shinri';
+import { machinaXFlayon } from './machina-x-flayon';
+
+export const holostarsEnGen2: LiverData[] = [
+  gavisBettel,
+  machinaXFlayon,
+  banzoinHakka,
+  josuijiShinri,
+];
---
+++ src/dictionary/holostars/en/gen2/machina-x-flayon.ts
@@ -0,0 +1,17 @@
+export const machinaXFlayon: LiverData = {
+  name: ['', ''],
+  alias: [
+    ['まきな', 'Machina'],
+    ['ふれおん', 'Flayon'],
+    ['まきなふれおん', 'Machina X Flayon'],
+  ],
+  marks: ['🪫'],
+  tags: [
+    ['#FlayOnAir', '生放送'],
+    ['#FlayonsCrayons', 'ファンアート'],
+    ['#MachironiXCheese', 'memes'],
+    ['#FlayOnWords', '切り抜き'],
+  ],
+  fans: ['Machiroons'],
+  twitter: ['@machinaxflayon'],
+};
