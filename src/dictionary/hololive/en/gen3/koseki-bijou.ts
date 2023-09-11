95e3cb7418d997c4cd621cf89a67e328376973a7
Yu MORIYA <euro21st@gmail.com>
2023-09-11 09:48:06 +0900

    Add EN gen3

---
+++ src/dictionary/hololive/en/gen3/koseki-bijou.ts
@@ -0,0 +1,18 @@
+export const kosekiBijou: LiverData = {
+  name: ['こせき びじゅう', '古石 ビジュー'],
+  alias: [
+    ['こせき', 'Koseki'],
+    ['びじゅう', 'Bijou'],
+    ['こせきびじゅう', 'Koseki Bijou'],
+  ],
+  marks: ['🗿'],
+  tags: [
+    ['#LIVEseki', '配信タグ'],
+    ['#LMOAI', 'ミーム'],
+    ['#bijouwled', 'ファンアート'],
+    ['#pebblesona', '石ころイラスト']
+  ],
+  fans: ['Pebbles'],
+  twitter: ['@kosekibijou'],
+  others: [],
+};
---
+++ src/dictionary/hololive/en/gen3/shiori-novella.ts
@@ -0,0 +1,22 @@
+export const shioriNovella: LiverData = {
+  name: ['しおり のヴぇら', 'シオリ ノベラ'],
+  alias: [
+    ['しおり', 'Shiori'],
+    ['のヴぇら', 'Novella'],
+    ['しおりのヴぇら', 'Shiori Novella'],
+    ['しおりのヴぇら', '収集家'],
+    ['しおりのヴぇら', 'The Archiver'],
+  ],
+  marks: ['👁️‍🗨️'],
+  tags: [
+    ['#ShiorinOnAir', '配信タグ'],
+    ['#ShiorinSketch', 'ファンアート'],
+    ['#ShiorinStories', 'ファン・フィクション'],
+  ],
+  fans: ['Novelites'],
+  twitter: ['@shiorinovella'],
+  others: [
+    ['advent', 'Advent'],
+    ['advent', 'Hololive English -Advent-']
+  ],
+};
