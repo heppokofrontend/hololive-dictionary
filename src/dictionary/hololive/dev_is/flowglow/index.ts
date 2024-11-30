6bf12f5897a406b856c266ed39e3bdcdd5e52647
Yu MORIYA <euro21st@gmail.com>
2024-11-30 19:54:41 +0900

    Add FLOW GLOW

---
+++ src/dictionary/hololive/dev_is/flowglow/index.ts
@@ -0,0 +1,13 @@
+import { isakiRiona } from "./isaki-riona";
+import { koganeiNiko } from "./koganei-niko";
+import { mizumiyaSu } from "./mizumiya-su";
+import { rindoChihaya } from "./rindo-chihaya";
+import { kikiraraVivi } from "./kikirara-vivi";
+
+export const hololiveDevIsFlowGlow: LiverData[] = [
+  isakiRiona,
+  koganeiNiko,
+  kikiraraVivi,
+  mizumiyaSu,
+  rindoChihaya,
+]
